2d27816c7245fa3f075a6e9e9295287c7c74c5ba
Benjamin Johnson <bbjohnson@health.ucsd.edu>
2026-01-26 08:54:44 -0800

    finalize_pairs script updates official pedigree

--- code/inst/hsw/hsw_breedHS_04_finalize_pairs.R
+++ code/inst/hsw/hsw_breedHS_04_finalize_pairs.R
@@ -20,35 +20,54 @@ utils <- basename(utils)
 setwd(code_dir)
 source(utils)
 
+cat(paste0('Producing final breeders file for HS West gen', generation), '\n')
+datestamp <- format(Sys.time(),'%Y%m%d-%H:%M:%S')
+cat(datestamp, '\n\n')
+
 # set up variables, paths, directories
 breedhs_outdir <- file.path(proj_dir, 'breedhs_out')
 altpair_outdir <- file.path(proj_dir, 'alt_pairings')
 kinship_outdir <- file.path(proj_dir, 'kinship')
-colony_dir <- file.path(proj_dir, 'colony')
+colony_dir <- file.path(proj_dir, 'colony_edits')
+hsw_raw_dir <- file.path(peds_dir, 'raw', 'hsw')
 pairs_proposed <- read.csv(pairs_proposed)
 kfile <- list.files(kinship_outdir, pattern='kinship_all_pairings', full.names=T)
+
 
 # read in any alternative pairings files filled out in the colony
 colony_files <- list.files(colony_dir, full.names=T)
 for (file in colony_files) {
     filestem <- basename(file)
     if (grepl('breeders_proposed', filestem)) {
+        cat('Using pairings file:', file, '\n\n')
         colony_pairs <- read.csv(file) } 
     if (grepl('alternative_pairs', filestem)) {
+        cat('Using alternative pairs file:', file, '\n')
         colony_alt_pairs <- read.csv(file) }
     if (grepl('replacement_pairs', filestem)) {
-        colony_rep_pairs <- read.csv(file) }
+        cat('Using replacement pairs file:', file, '\n')
+       colony_rep_pairs <- read.csv(file) }
     if (grepl('new_pairs', filestem)) {
+        cat('Using novel pairs file:', file, '\n')
         colony_new_pairs <- read.csv(file) }
 }
 
-if(!exists('colony_pairs')) {
-    cat('Please provide the pairings file used in the colony \n')
+if (!exists('colony_pairs')) {
+    cat('ERROR: No pairings file. Please provide the pairings file used in the colony \n')
     quit(status=1)
 }
-if(!exists('colony_alt_pairs')) { colony_alt_pairs <- NULL}
-if(!exists('colony_rep_pairs')) { colony_rep_pairs <- NULL}
-if(!exists('colony_new_pairs')) { colony_new_pairs <- NULL}
+if (!exists('colony_alt_pairs')) { 
+    colony_alt_pairs <- NULL
+    cat('No alternative pairs file used \n')
+    }
+if (!exists('colony_rep_pairs')) { 
+    colony_rep_pairs <- NULL
+    cat('No replacement pairs file used \n')
+    }
+if (!exists('colony_new_pairs')) { 
+    cat('No novel pairs file used \n')
+    colony_new_pairs <- NULL
+    }
 
 
 # update the breeder file with colony edits
@@ -63,6 +82,7 @@ breedpairs <- final_breeder_file(
     pop = pop,
     gen = generation,
     outdir = proj_dir)
+
 
 # plot kinship 
 printout('Plotting pairwise kinship for all breeder pairings')
@@ -88,5 +108,23 @@ plot_k_network(
     sample = 'breederpairs',
     out_dir = kinship_outdir)
 
+
+# finalize the current generation's pedigree
+printout(paste0('Producing the finalized raw pedigree for gen', generation))
+
+hsw_raw_ped <- final_hsw_breeders_to_raw_ped(
+    pairs = breedpairs,
+    colony_df = colony_df,
+    prev_df = prev_colony_df,
+    stem = hsw_raw_stem,
+    outdir = hsw_raw_dir)
+
+printout(paste0('Producing the finalized raw pedigree for gen', generation))
+concat_peds(
+    directory = hsw_raw_dir,
+    stem = hsw_raw_stem,
+    outdir = hsw_raw_dir,
+    outstem = 'hsw_raw_ped_complete',
+    return_df = FALSE)
 
 printout('Breederpair updates complete!')
